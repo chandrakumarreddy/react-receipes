2fcd164a584d1b7813216cf75fc14e613abf876a
chandrakumarreddy <chandrapenugonda655@gmail.com>
2020-01-26 19:59:28 +0530

    add react router dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 
-import "./index.css";
 import App from "./components/App";
+import Signin from "./components/Auth/Signin";
+import Signup from "./components/Auth/Signup";
+import "./index.css";
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/signin" component={Signin} />
+      <Route path="/signup" component={Signup} />
+    </Switch>
+  </Router>
+);
 
 const client = new ApolloClient({
   uri: "http://localhost:4444/graphql"
@@ -12,7 +25,7 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Root />
   </ApolloProvider>,
   document.getElementById("root")
 );
